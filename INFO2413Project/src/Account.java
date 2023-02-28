0e1b457729e0539c2c8ef88679c4d3faa0df805f
AnVu1709 <chenaholic99er@gmail.com>
2023-02-27 17:37:47 -0800

    first commit

--- INFO2413Project/src/Account.java
+++ INFO2413Project/src/Account.java
@@ -31,5 +31,35 @@ public class Account {
     public void viewLabExam() {
     	
     }
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
 
