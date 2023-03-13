39a93a81dd370c5c95843fbcc038bdedcb83fcf8
ivanprovci <ivanprovci@yahoo.com>
2023-03-13 16:09:50 -0700

    enabled CORS + added login page for testing

--- src/main/java/com/kpu/student/Project/Application.java
+++ src/main/java/com/kpu/student/Project/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Application {
     public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
