c3d10f228f43680d9b3542e44c7c1e6aa4b4e702
liamharder <56138157+liamharder@users.noreply.github.com>
2023-03-20 21:31:20 -0700

    Start work on retrieveAccountInfo()

--- src/main/java/com/kpu/student/Project/DatabaseAccessor.java
+++ src/main/java/com/kpu/student/Project/DatabaseAccessor.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 public class DatabaseAccessor {
 
 	public static String retrievePasswordHash(String email) {
+		//Gets the password hash associated with a certain email
+		//Returns an empty string if there is no account associated with the email - Liam
 		String pwHash = "";
 		Connection c = DatabaseAccessor.connect();
 		try {
@@ -16,7 +18,7 @@ public class DatabaseAccessor {
 					"SELECT passwordHash FROM Account WHERE email = ?");
 			getPW.setString(1, email);
 			ResultSet results = getPW.executeQuery();
-			pwHash = results.getString(0);
+			pwHash = results.getString("passwordHash");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -26,6 +28,49 @@ public class DatabaseAccessor {
 	}
 	public static Account retrieveAccountInfo(String email) {
 		Account retrievedAccount = new Account("", "");
+		Connection c = DatabaseAccessor.connect();
+		try {
+			PreparedStatement checkIfStaff = c.prepareStatement(
+					"SELECT * FROM StaffAccount WHERE email = ?");
+			checkIfStaff.setString(1, email);
+			ResultSet staffResults = checkIfStaff.executeQuery();
+			
+			PreparedStatement checkIfDoctor = c.prepareStatement(
+					"SELECT * FROM DoctorAccount WHERE email = ?");
+			checkIfDoctor.setString(1, email);
+			ResultSet doctorResults = checkIfDoctor.executeQuery();
+			
+			PreparedStatement checkIfPatient = c.prepareStatement(
+					"SELECT * FROM PatientAccount WHERE email = ?");
+			checkIfPatient.setString(1, email);
+			ResultSet patientResults = checkIfPatient.executeQuery();
+			
+			if (staffResults.getString(1) != null) {
+				retrievedAccount = new StaffAccount(email, 
+						DatabaseAccessor.retrievePasswordHash(email));				
+				
+			} else if (doctorResults.getString(1) != null) {
+				retrievedAccount = new DoctorAccount(email, 
+						DatabaseAccessor.retrievePasswordHash(email));
+				
+			} else if (patientResults.getString(1) != null) {
+				retrievedAccount = new PatientAccount(email, 
+						DatabaseAccessor.retrievePasswordHash(email));
+				
+			}
+			
+			PreparedStatement generalAccountInfo = c.prepareStatement(
+					"SELECT * FROM Account WHERE email = ?");
+			checkIfPatient.setString(1, email);
+			ResultSet accountResults = generalAccountInfo.executeQuery();
+			retrievedAccount.setFirstName(accountResults.getString("firstName"));
+			retrievedAccount.setLastName(accountResults.getString("lastName"));
+			retrievedAccount.setPhoneNo(accountResults.getString("phoneNo"));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return retrievedAccount;
 	}
