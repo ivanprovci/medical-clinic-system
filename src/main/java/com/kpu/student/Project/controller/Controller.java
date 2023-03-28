87c86fbfe2226da56d8120cf609c8a6fa3c79c32
liamharder <56138157+liamharder@users.noreply.github.com>
2023-03-28 13:12:30 -0700

    Made test /getRecords a little clearer

--- src/main/java/com/kpu/student/Project/controller/Controller.java
+++ src/main/java/com/kpu/student/Project/controller/Controller.java
@@ -1,10 +1,13 @@
 package com.kpu.student.Project.controller;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.kpu.student.Project.ConfidentialRecord;
@@ -16,16 +19,22 @@ import com.kpu.student.Project.VisitRecord;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Controller {
 	
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!!!!!", name);
     }
     
-    @GetMapping("/getRecords")
-    public List<ConfidentialRecord> hello2(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @RequestMapping("/getRecords")
+    public List<ConfidentialRecord> getRecords(@RequestParam(value = "email", defaultValue = "") String requestingEmail) {
     	List<ConfidentialRecord> recordList = new ArrayList<ConfidentialRecord>();
     	ConfidentialRecord record1 = new VisitRecord(0, "steve@email.com", "steve-doctor@email.com");
+    	((VisitRecord) record1).setVisitDate(new Date(0));
     	ConfidentialRecord record2 = new Prescription(1, (VisitRecord) record1);
+    	((Prescription) record2).setDatePrescribed(new Date(0));
+    	((Prescription) record2).setMedicineDose("10ml");
+    	((Prescription) record2).setMedicineName("Cough Syrup");
+    	((Prescription) record2).setMedicineQuantity("250ml bottle");
+    	((Prescription) record2).setRefillable(true);
     	recordList.add(record1);
     	recordList.add(record2);
     	return recordList;
@@ -33,7 +42,7 @@ public class Controller {
 	 
     //Sends an email/hashed password combo to be checked
     //Returns true if the combination is valid, false otherwise - Liam
-    @GetMapping("/checkPassword")
+    @PostMapping("/checkPassword")
     public Boolean checkPassword(@RequestParam(value = "email", defaultValue = "") String email,
     		@RequestParam(value = "password", defaultValue = "") String password) {
     	String s = DatabaseAccessor.retrievePasswordHash(email);
