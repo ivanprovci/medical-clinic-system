9f4cde2cdd3b9928ae84716f73f86f727b7dd095
AnVu1709 <122769461+AnVu1709@users.noreply.github.com>
2023-03-28 23:33:14 -0700

    Update Account.java

--- src/main/java/com/kpu/student/Project/Account.java
+++ src/main/java/com/kpu/student/Project/Account.java
@@ -1,4 +1,9 @@
 package com.kpu.student.Project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class Account {
 
 	private String email;
