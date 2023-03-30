e5045d7e61071c94aef6ea56d5030c6d46e4a7b2
liamharder <56138157+liamharder@users.noreply.github.com>
2023-03-29 22:50:53 -0700

    Implement /getRecords

--- src/main/java/com/kpu/student/Project/controller/Controller.java
+++ src/main/java/com/kpu/student/Project/controller/Controller.java
@@ -36,25 +36,19 @@ public class Controller {
     }
     
     @RequestMapping("/getRecords")
-    public List<ConfidentialRecord> getRecords(@RequestParam(value = "email", defaultValue = "") String requestingEmail) {
+    public List<ConfidentialRecord> getRecords(@RequestParam(value = "email", required = true) String requestingEmail) {
     	List<ConfidentialRecord> recordList = new ArrayList<ConfidentialRecord>();
-    	ConfidentialRecord record1 = new VisitRecord(0, "steve@email.com", "steve-doctor@email.com");
-    	((VisitRecord) record1).setVisitDate(new Date(0));
-    	ConfidentialRecord record2 = new Prescription(1, (VisitRecord) record1);
-    	((Prescription) record2).setDatePrescribed(new Date(0));
-    	((Prescription) record2).setMedicineDose("10ml");
-    	((Prescription) record2).setMedicineName("Cough Syrup");
-    	((Prescription) record2).setMedicineQuantity("250ml bottle");
-    	((Prescription) record2).setRefillable(true);
-    	((VisitRecord) record1).addExam(new LabExam(6));
-    	((VisitRecord) record1).addExam(new LabExam(7));
-    	((VisitRecord) record1).addExam(new LabExam(8));
-    	((VisitRecord) record1).addExam(new LabExam(9));
-    	((VisitRecord) record1).addPrescription(new Prescription(15));
-    	((VisitRecord) record1).addPrescription(new Prescription(16));
-    	((VisitRecord) record1).addPrescription(new Prescription(17));
-    	recordList.add(record1);
-    	recordList.add(record2);
+    	
+		try {
+			List<Integer> idList = DatabaseAccessor.getRecordsForAccount(requestingEmail);
+			for (Integer i : idList) {
+	    		recordList.add(DatabaseAccessor.retrieveRecord(i));
+	    	}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     	return recordList;
     }
 	 
